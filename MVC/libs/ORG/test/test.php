273cec613ec234ca926bfc2dbc8dd6a087893d6f
JS <350172898@qq.com>
2016-09-18 23:15:26 +0800

    MVC

--- MVC/libs/ORG/test/test.php
+++ MVC/libs/ORG/test/test.php
@@ -12,6 +12,15 @@
       $smarty -> caching         = true;        //开启缓存
       $smarty -> cache_lifetime  = 120;         //缓存开启时间
 
-      $smarty -> assign('articletitle','文章标题');
+      //$smarty -> assign('articletitle','i ate an apple');
+      $smarty -> assign('time', time());
+      //$smarty -> assign('articletitle', '');
+      // class My_Object{
+      //       function meth1($params){
+      //             return $params[0]."已经".$params[1];
+      //       }
+      // }
+      // $myobj = new My_Object();
+      // $smarty -> assign('myobj',$myobj);
       $smarty -> display('test.tpl');
 ?>
--- MVC/libs/ORG/test/tpl/test.tpl
+++ MVC/libs/ORG/test/tpl/test.tpl
@@ -1,1 +1,7 @@
-{$articletitle}
+{*$articletitle|capitalize*}
+{*$articletitle|cat:" yesterday"*}
+{*$time|date_format:"%H:%M:%S %B %e %Y"*}
+{*$articletitle|default:"no title"*}
+{*include file = "header.tpl" sitname1 ="1慕课网"*}
+{*$myobj->meth1(array('苹果','熟了'))*}
+{"Y-m-d"|date:$time}
