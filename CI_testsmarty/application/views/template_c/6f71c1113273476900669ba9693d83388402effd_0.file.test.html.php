92da59492e6beb70b4979ddae7375b204e8dc504
syjs10 <350172898@qq.com>
2017-01-27 19:43:53 +0800

    CI框架整合Smarty

---
+++ CI_testsmarty/application/views/template_c/6f71c1113273476900669ba9693d83388402effd_0.file.test.html.php
@@ -0,0 +1,30 @@
+<?php
+/* Smarty version 3.1.30, created on 2017-01-27 19:26:26
+  from "/var/www/html/CI_testsmarty/application/views/test.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_588b2e62371174_38690573',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '6f71c1113273476900669ba9693d83388402effd' => 
+    array (
+      0 => '/var/www/html/CI_testsmarty/application/views/test.html',
+      1 => 1485516375,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_588b2e62371174_38690573 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+<?php echo $_smarty_tpl->tpl_vars['test']->value;?>
+
+</body>
+</html><?php }
+}
