fe9c0d342007312522249b7d169983f05f57c66b
Js <350172898@qq.com>
2017-03-01 16:29:24 +0800

    learntest

---
+++ testfunc/1-str.php
@@ -0,0 +1,73 @@
+<!-- <?php  
+	$str = "qwertyuiopasdfghjklzxcvbnm1234567890QWERTYUIOPASDFGHJKLZXCVBNM";
+	$i=0;
+	$string='';
+	while ($i++<4){
+		$string .= $str{mt_rand(0, strlen($str))};
+	}
+	echo $string;
+?> -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<style type="text/css">
+		.foo{
+			width: 100px;
+			@f-
+			height: 5px;
+			margin-right: 5px;
+			@font-face {
+				font-family: 'FontName';
+				src: url('FileName.eot');
+				src: url('FileName.eot?#iefix') format('embedded-opentype'),
+					 url('FileName.woff') format('woff'),
+					 url('FileName.ttf') format('truetype'),
+					 url('FileName.svg#FontName') format('svg');
+				font-style: normal;
+				font-weight: normal;
+			}
+			-webkit-transition: prop time;
+			-o-transition: prop time;
+			transition: prop time;
+		}
+		body {
+			background-image: -webkit-linear-gradient(left, #666 50%, #fff);
+			background-image: -o-linear-gradient(left, #666 50%, #fff);
+			background-image: linear-gradient(to right, #666 50%, #fff);
+		}
+	</style>
+</head>
+<body>
+	<p></p>
+	<div></div>
+	<div class="foo">
+		<h1></h1>
+	</div>
+	<div class="bar">
+		<h2></h2>
+	</div>
+	<table>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+	</table>
+	<div class="foot">
+		
+	</div>
+	<div id="header" class="c">
+		<h1></h1>
+		<p></p>
+	</div>
+	<h2></h2>
+	<h3></h3>
+
+
+
+</body>
+</html>
